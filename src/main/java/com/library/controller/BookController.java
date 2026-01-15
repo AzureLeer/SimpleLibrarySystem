63c093fd00e6f066dac23b33a782ea17a8cc11f2
Azure <2216197183@qq.com>
2026-01-15 09:57:08 +0800

    依赖框架+java类

--- src/main/java/com/library/controller/BookController.java
+++ src/main/java/com/library/controller/BookController.java
@@ -1,22 +1,29 @@
 package com.library.controller;
 
 import com.library.bean.Book;
+import com.library.bean.Lend;
+import com.library.bean.ReaderCard;
 import com.library.service.BookService;
 import com.library.service.LendService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 /**
  * 接收前端图书http请求
- *包括管理员/读者视角
+ * 包括管理员/读者视角
  * 图书查询、添加、修改、详情查看
  * 读者借阅
  */
@@ -28,30 +35,128 @@ public class BookController {
     private LendService lendService;
 
     //前端日期转换
-    private Date getDate(String pubstr){
-        try{
+    private Date getDate(String pubstr) {
+        try {
             SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
             return df.parse(pubstr);
-        }catch (ParseException e){
+        } catch (ParseException e) {
             e.printStackTrace();
             return new Date();
         }
     }
 
+    //图书查询
     @RequestMapping("/querybook.html")
     public ModelAndView queryBookDo(String searchWord) {
-        if(bookService.matchBook(searchWord)) {
+        if (bookService.matchBook(searchWord)) {
             ArrayList<Book> books = bookService.queryBook(searchWord);
             ModelAndView modelAndView = new ModelAndView("admin_books");
             modelAndView.addObject("books", books);
             return modelAndView;
-        }else{
-            return new ModelAndView("admin_books","error","没有匹配的图书");
+        } else {
+            return new ModelAndView("admin_books", "error", "没有匹配的图书");
         }
 
     }
 
+    //管理员图书视图
+    @RequestMapping("/admin_books.html")
+    public ModelAndView adminBooks() {
+        ArrayList<Book> books = bookService.getAllBooks();
+        ModelAndView modelAndView = new ModelAndView("admin_books");
+        modelAndView.addObject("books", books);
+        return modelAndView;
+    }
 
+    //管理员添加图书
+    @RequestMapping("/book_add.html")
+    public ModelAndView addbook() {
+        return new ModelAndView("admin_book_add");
+    }
 
+    //图书添加结果
+    @RequestMapping("/book_add_do.html")
+    public String addbookDo(@RequestParam(value = "pubstr") String pubstr, Book book, RedirectAttributes redirectAttributes) {
+        book.setPub_date(getDate(pubstr));
+        if (bookService.addBook(book)) {
+            redirectAttributes.addFlashAttribute("succ", "图书添加成功！");
+        } else {
+            redirectAttributes.addFlashAttribute("error", "图书添加失败！");
+        }
+        return "redirect:/admin_books.html";
+    }
+
+    //管理员编辑图书
+    @RequestMapping("/updatebook.html")
+    public ModelAndView bookEdit(HttpServletRequest request, Model model) {
+        long bookId = Long.parseLong(request.getParameter("bookId"));
+        Book book = bookService.getBook(bookId);
+        ModelAndView modelAndView = new ModelAndView("admin_book_edit");
+        modelAndView.addObject("detail", book);
+        return modelAndView;
+    }
+
+    //图书编辑结果
+    @RequestMapping("/book_edit_do.html")
+    public String bookEditDo(@RequestParam(value = "pubstr") String pubstr, Book book, RedirectAttributes redirectAttributes) {
+        book.setPub_date(getDate(pubstr));
+        if (bookService.editBook(book)) {
+            redirectAttributes.addFlashAttribute("succ", "图书修改成功！");
+        } else {
+            redirectAttributes.addFlashAttribute("error", "图书修改失败！");
+        }
+        return "redirect:/admin_books.html";
+    }
+
+    //管理员图书详情
+    @RequestMapping("/admin_book_detail.html")
+    public ModelAndView adminBookDetail(HttpServletRequest request, Model model) {
+        long bookId = Long.parseLong(request.getParameter("bookId"));
+        Book book = bookService.getBook(bookId);
+        ModelAndView modelAndView = new ModelAndView("admin_book_detail");
+        modelAndView.addObject("detail", book);
+        return modelAndView;
+    }
+
+    //读者图书详情
+    @RequestMapping("/reader_book_detail.html")
+    public ModelAndView readerBookDetail(HttpServletRequest request, Model model) {
+        long bookId = Long.parseLong(request.getParameter("bookId"));
+        Book book = bookService.getBook(bookId);
+        ModelAndView modelAndView = new ModelAndView("reader_book_detail");
+        modelAndView.addObject("detail", book);
+        return modelAndView;
+    }
+
+    //管理员主页
+    @RequestMapping("/admin_header.html")
+    public ModelAndView admin_Header() {
+        return new ModelAndView("admin_header");
+    }
+
+    //读者主页
+    @RequestMapping("/reader_header.html")
+    public ModelAndView reader_Header() {
+        return new ModelAndView("reader_header");
+    }
+
+    //读者图书归还列表
+    @RequestMapping("reader_books.html")
+    public ModelAndView readerBooks(HttpServletRequest request) {
+        ArrayList<Book> books = bookService.getAllBooks();
+        ReaderCard readerCard = (ReaderCard) request.getSession().getAttribute("readerCard");
+        ArrayList<Lend> myAllLendList = lendService.myLendList(readerCard.getReader_id());
+        ArrayList<Long> myLendList = new ArrayList<>();
+        for(Lend lend : myAllLendList){
+            //是否归还
+            if(lend.getBack_date()==null){
+                myLendList.add(lend.getBook_id());
+            }
+        }
+        ModelAndView modelAndView = new ModelAndView("reader_books");
+        modelAndView.addObject("books", books);
+        modelAndView.addObject("myLendList", myLendList);
+        return modelAndView;
+    }
 
 }
--- src/main/java/com/library/controller/LendController.java
+++ src/main/java/com/library/controller/LendController.java
@@ -1,4 +1,90 @@
 package com.library.controller;
 
+import com.library.bean.ReaderCard;
+import com.library.service.BookService;
+import com.library.service.LendService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@Controller
 public class LendController {
+    @Autowired
+    private LendService lendService;
+    @Autowired
+    private BookService bookService;
+
+    //删除图书
+    @RequestMapping("/delete_book.html")
+    public String deleteBook(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        long bookId = Long.parseLong(request.getParameter("bookId"));
+        if (bookService.deleteBook(bookId)) {
+            redirectAttributes.addFlashAttribute("succ", "图书删除成功！");
+        } else {
+            redirectAttributes.addFlashAttribute("error", "图书删除失败！");
+        }
+        return "redirect:/admin_books.html";
+    }
+
+    //借阅列表
+    @RequestMapping("/lendlist.html")
+    public ModelAndView lendList(HttpServletRequest request) {
+
+        ModelAndView modelAndView = new ModelAndView("admin_lend_list");
+        modelAndView.addObject("list", lendService.lendList());
+        return modelAndView;
+    }
+
+    //我的借阅
+    @RequestMapping("/mylend.html")
+    public ModelAndView myLend(HttpServletRequest request) {
+        ReaderCard readerCard = (ReaderCard) request.getSession().getAttribute("readerCard");
+        ModelAndView modelAndView = new ModelAndView("reader_my_lend");
+        modelAndView.addObject("list", lendService.myLendList(readerCard.getReader_id()));
+        return modelAndView;
+    }
+
+    //删除借阅
+    @RequestMapping("/deletelend.html")
+    public String deleteLend(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        long serNum = Long.parseLong(request.getParameter("serNum"));
+        if (lendService.deleteLend(serNum)) {
+            redirectAttributes.addFlashAttribute("succ", "记录删除成功！");
+        } else {
+            redirectAttributes.addFlashAttribute("error", "记录删除失败！");
+        }
+        return "redirect:/lendlist.html";
+    }
+
+    //借阅图书
+    @RequestMapping("/lendbook.html")
+    public String bookLend(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        long bookId = Long.parseLong(request.getParameter("bookId"));
+        long readerId = ((ReaderCard) request.getSession().getAttribute("readerCard")).getReader_id();
+        if (lendService.lendBook(bookId, readerId)) {
+            redirectAttributes.addFlashAttribute("succ", "图书借阅成功！");
+        } else {
+            redirectAttributes.addFlashAttribute("error", "图书借阅失败！");
+        }
+        return "redirect:/reader_books.html";
+    }
+
+    //归还图书
+    @RequestMapping("/returnbook.html")
+    public String bookReturn(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        long bookId = Long.parseLong(request.getParameter("bookId"));
+        long readerId = ((ReaderCard) request.getSession().getAttribute("readerCard")).getReader_id();
+        if (lendService.returnBook(bookId, readerId)) {
+            redirectAttributes.addFlashAttribute("succ", "图书归还成功！");
+        } else {
+            redirectAttributes.addFlashAttribute("error", "图书归还失败！");
+        }
+        return "redirect:/reader_books.html";
+    }
+
 }
